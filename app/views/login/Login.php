997c769e5aac0a7f4eed575f52a84ad2e5a8d590
andreshaitit <46727423+andreshaitit@users.noreply.github.com>
2019-07-26 21:30:49 -0300

    Front_end
    Pagina de error nuevoa- login-header-footer

--- app/views/login/Login.php
+++ app/views/login/Login.php
@@ -1,84 +1,48 @@
+
 <!doctype html>
-<html lang="es">
+<html lang="en">
+  <head>
+    <title>Login</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
-  <meta name="generator" content="Jekyll v3.8.5">
-  <title>Iniciar Sesion</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  </head>
+  <body>
 
-  <link rel="canonical" href="https://getbootstrap.com/docs/4.3/examples/sign-in/">
+  <div class="container mt-5">
+    <div class="row">
+      <div class="offset-4 col-4">
+      <div class="card">
+  <img class="card-img-top" src="">
+  <div class="card-body">
+    <h4 class="card-title text-center">Inicia Sesion</h4>
+    <p class="card-text">
+    <form >
+  <div class="form-group">
+    <label>Usuario</label>
+    <input type="text" class="form-control" placeholder="">
+  </div>
+  <div class="form-group">
+    <label>Contraseña</label>
+    <input type="text" class="form-control" placeholder="">
+  </div>
+</form>
 
-  <!-- Bootstrap core CSS -->
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
-  <style type="text/css">
-    html,
-    body {
-      height: 100%;
-    }
-
-    body {
-      display: -ms-flexbox;
-      display: flex;
-      -ms-flex-align: center;
-      align-items: center;
-      padding-top: 40px;
-      padding-bottom: 40px;
-      background-color: #f5f5f5;
-    }
-
-    .form-signin {
-      width: 100%;
-      max-width: 330px;
-      padding: 15px;
-      margin: auto;
-    }
-
-    .form-signin .checkbox {
-      font-weight: 400;
-    }
-
-    .form-signin .form-control {
-      position: relative;
-      box-sizing: border-box;
-      height: auto;
-      padding: 10px;
-      font-size: 16px;
-    }
-
-    .form-signin .form-control:focus {
-      z-index: 2;
-    }
-
-    .form-signin input[type="email"] {
-      margin-bottom: -1px;
-      border-bottom-right-radius: 0;
-      border-bottom-left-radius: 0;
-    }
-
-    .form-signin input[type="password"] {
-      margin-bottom: 10px;
-      border-top-left-radius: 0;
-      border-top-right-radius: 0;
-    }
-  </style>
-
-</head>
-
-<body class="text-center">
-  <form class="form-signin" method="POST" action="<?= FOLDER_PATH . 'login/sigin' ?>">
-    <img class="mb-4" src="/docs/4.3/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
-    <h1 class="h3 mb-3 font-weight-normal">Por favor inicie sesion</h1>
-    <label for="inputEmail" class="sr-only">Email address</label>
-    <input type="email" id="" name="email" class="form-control" placeholder="Email" required autofocus>
-    <label for="inputPassword" class="sr-only">Password</label>
-    <input type="password" id="" name="password" class="form-control" placeholder="Contraseña" required>
-    <?php !empty($_SESSION['error_login']) ? print($_SESSION['error_login']) : ''; ?>
-    <button class="btn btn-lg btn-primary btn-block" type="submit">Iniciar Sesion</button>
-  </form>
-</body>
-
+    </p>
+    <button type="submit" class="btn btn-primary">Iniciar</button>
+  </div>
+</div>
+      </div>
+    </div>
+  </div>
+      
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
 </html>
