346f6a84e106526c08ecfe241e9b25a53dcd14f8
JaviLerma <41553977+JaviLerma@users.noreply.github.com>
2019-10-01 22:26:26 -0300

    Update clinica.sql

--- clinica.sql
+++ clinica.sql
@@ -340,7 +340,7 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`id`, `nombre_usuario`, `nombre`, `apellido`, `email`, `password`, `rol`, `imagen`) VALUES
-(1, 'admin', 'Admin', 'Admin', 'admin@admin.com', 'c7ad44cbad762a5da0a452f9e854fdc1e0e7a52a38015f23f3eab1d80b931dd472634dfac71cd34ebc35d16ab7fb8a90c81f975113d6c7538dc69dd8de9077ec', 'admin', NULL);
+(1, 'admin', 'Admin', 'Admin', 'admin@admin.com', '$2y$10$IaHs0BIkMwRFOw/lLfWLae9NHFGID.DKEFpN86nE1tYywUKokWJ/e', 'admin', NULL);
 
 --
 -- Índices para tablas volcadas
