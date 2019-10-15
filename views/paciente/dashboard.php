12aa7dbd733179a6207e3ce1a1161932d2fb86ab
Damian <damiancanonica90@gmail.com>
2019-10-15 15:37:35 -0300

    update

--- views/paciente/dashboard.php
+++ views/paciente/dashboard.php
@@ -13,7 +13,7 @@
 			<div class="col-12">
 
 				<?php if (isset($pac) && is_object($pac)) : ?>
-					<h1 class="text-center my-4">Detalles Paciente <?= $pac->nombre . " ".$pac->apellido  ?></h1>
+					<h1 class="text-center my-4">Detalles Paciente <hr> </h1>
 			
 				<?php 
 					else : 
@@ -22,5 +22,62 @@
 				?>
 			</div>
 		</div>
+
+<!-- container pedidos mas columna paciente-->
+<div class="container">
+  <div class="row">
+    <div class="col-md-auto">
+      <!-- Pedidos de los pacientes-->
+	  <h1 style="text-align:right">Pedidos</h1>
+	  <p></p>
+    </div>
+    <div class="col-sm">
+      <!-- One of two columns -->
+    </div>
+    <div class="col-md-auto">
+	<!-- columna de datos del paciente -->
+	<table class="table table-striped table-hover text-right">
+					<tbody>
+					<?php
+							require_once 'models/obrasociales.php';
+							$obrasocial = new ObraSocial();
+							?>
+						<tr>
+						<h3 class="text-right"><?= $pac->nombre . " ".$pac->apellido  ?></h2>
+						</tr>
+						<tr>
+								<td scope="row"><?php echo "Paciente Nº " . "" . $pac->id_paciente; ?></td>
+						</tr>
+						<tr>
+								<td><?= $pac->apellido . ", " . $pac->nombre; ?></td>
+						</tr>
+								<td><?= $pac->dni; ?></td>
+						<tr>
+								<td>
+									<?php
+										$cumpleanos = new DateTime($pac->fecha_nacimiento);
+										$hoy = new DateTime();
+										$annos = $hoy->diff($cumpleanos);
+										echo "Edad:" . " " . $annos->y;;
+									?>
+								</td>
+						</tr>
+						<tr>
+								<td><?= $pac->sexo; ?></td>
+						</tr>
+						<tr>
+							<td>
+							<?php
+								$obra = $obrasocial->getObraSocial($pac->id_paciente);
+							    		$numero_socio = $obrasocial->getNumeroObraSocial($pac->id_paciente);
+										echo $obra->nombre . " | Numero: " . $numero_socio->numero_socio;
+							?>
+						</tr>
+						</tbody>
+						</table>
+    </div>
+  </div>
+</div>
+								
 
 <?php endif; ?>
--- views/pedido/gestion.php
+++ views/pedido/gestion.php
@@ -1,10 +1,16 @@
-<?php if(isset($_SESSION['identity'])): ?>
-<div class="col-10">
-<?php if (isset($gestion)): ?>
-	<h1 class="text-center my-4">Gestionar atenciones</h1>
-<?php else: ?>
-	<h1 class="text-center my-4">Atenciones</h1>
-<?php endif; ?>
+<?php if (isset($_SESSION['identity'])) : ?>
+	<div class="col-10">
+		<div class="row">
+
+			<div class="col-12">
+
+				.<div class="row">
+					<div class="col-12">
+
+
+						<h1 class="text-center my-4">Gestión de Pedidos de atención</h1>
+					</div>
+
 <table class="table table-bordered">
   <thead>
 	<tr>
